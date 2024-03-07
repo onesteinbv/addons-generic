2fb6cb467e27c9a9e2088b4a92799d18ec477392
tarteo <d.sluijk@onestein.nl>
2024-03-07 12:09:47 +0100

    Move rgs modules

---
+++ account_asset_management_template/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Asset management template",
+    "version": "16.0.1.0.0",
+    "category": "Accounting & Finance",
+    "author": "Onestein",
+    "website": "https://www.onestein.nl",
+    "depends": [
+        "account_asset_management",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "license": "LGPL-3",
+}
