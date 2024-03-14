acfcb015914fa8d2fe414fe75a42a962ae84d9df
tarteo <d.sluijk@onestein.nl>
2024-03-14 14:57:51 +0100

    website_sale_recurring_payment: Move compatible provider filter to sale_recurring_payment
    and remove website_sale_recurring_payment

--- sale_recurring_payment/models/payment_provider.py
+++ sale_recurring_payment/models/payment_provider.py
@@ -1,7 +1,16 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PaymentProvider(models.Model):
     _inherit = "payment.provider"
 
     allows_recurring_payment = fields.Boolean()
+
+    @api.model
+    def _get_compatible_providers(self, *args, **kwargs):
+        providers = super()._get_compatible_providers(*args, **kwargs)
+        if kwargs.get("sale_order_id"):
+            sale_order = self.env["sale.order"].sudo().browse(kwargs["sale_order_id"])
+            if sale_order and sale_order.group_subscription_lines():
+                providers = providers.filtered(lambda p: p.allows_recurring_payment)
+        return providers
--- website_sale_recurring_payment/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    "name": "Website Sale - Recurring Payment",
-    "version": "16.0.0.1",
-    "category": "eCommerce",
-    "license": "LGPL-3",
-    "summary": "Website Sale - Recurring Payment",
-    "website": "https://www.onestein.nl",
-    "depends": ["website_sale", "sale_recurring_payment"],
-}
