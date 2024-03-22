18179343d5870c05767d1a4c7aa69b3c949897d2
André Schenkels <andre@schenkels.nl>
2024-03-22 12:37:35 +0000

    extra pre-commit fixes

--- l10n_nl_rgs/models/account_group.py
+++ l10n_nl_rgs/models/account_group.py
@@ -14,19 +14,21 @@ class AccountGroup(models.Model):
     code = fields.Char()
     sort_code = fields.Char(string="Sorting code")
     allowed_journal_ids = fields.Many2many(
-        comodel_name="account.journal", string="Saved Allowed Journals",
-        help="Define in which journals this account can be used. If empty, can be used in all journals."
+        comodel_name="account.journal",
+        string="Saved Allowed Journals",
+        help="Define in which journals this account can be used. If empty, can be used in all journals.",
     )
     active_allowed_journal_ids = fields.Many2many(
-        comodel_name="account.journal", string="Allowed Journals",
+        comodel_name="account.journal",
+        string="Allowed Journals",
         help="This is the allowed journal for this group. It's calculated from all parent_groups",
-        compute="compute_active_allowed_journals",
-        inverse="set_allowed_journals"
+        compute="_compute_active_allowed_journals",
+        inverse="_inverse_set_allowed_journals",
     )
     auto_allowed_journals = fields.Boolean(
         string="Automatic Allowed Journals",
         default=True,
-        help="If Automatic Allowed Journals is on. Changes here will be brought to the underlying accounts."
+        help="If Automatic Allowed Journals is on. Changes here will be brought to the underlying accounts.",
     )
     # From account financial report
     group_child_ids = fields.One2many(
@@ -35,7 +37,7 @@ class AccountGroup(models.Model):
     account_ids = fields.One2many(
         comodel_name="account.account", inverse_name="group_id", string="Accounts"
     )
-    
+
     def _adapt_parent_account_group(self):
         if self.company_id.chart_template_id != self.env.ref(
             "l10n_nl_rgs.l10nnl_rgs_chart_template", False
@@ -49,7 +51,7 @@ class AccountGroup(models.Model):
             if rec.group_child_ids:
                 accounts |= rec.group_child_ids.get_all_account_ids()
         return accounts
-    
+
     def get_all_allowed_journal_ids(self):
         allowed_journals = self.env["account.journal"]
         for rec in self:
@@ -63,25 +65,27 @@ class AccountGroup(models.Model):
         # Always check the allowed journals if auto_allowed_journals
         self.accounts_set_allowed_journals()
         return ret
-    
+
     @api.model_create_multi
     def create(self, vals_list):
         records = super().create(vals_list)
         records.accounts_set_allowed_journals()
         return records
-    
+
     def accounts_set_allowed_journals(self):
         for rec in self:
             if rec.auto_allowed_journals:
-                rec.with_context(group_allowed_journal_change=True).account_ids.allowed_journal_ids = rec.active_allowed_journal_ids
+                rec.with_context(
+                    group_allowed_journal_change=True
+                ).account_ids.allowed_journal_ids = rec.active_allowed_journal_ids
 
-    @api.depends('parent_id','parent_id.allowed_journal_ids')
-    @api.onchange('parent_id', 'allowed_journal_ids')
-    def compute_active_allowed_journals(self):
+    @api.depends("parent_id", "parent_id.allowed_journal_ids")
+    @api.onchange("parent_id", "allowed_journal_ids")
+    def _compute_active_allowed_journals(self):
         for rec in self:
             rec.active_allowed_journal_ids = rec.get_all_allowed_journal_ids()
 
-    def set_allowed_journals(self):
+    def _inverse_set_allowed_journals(self):
         for rec in self:
             parent_journals = rec.parent_id.get_all_allowed_journal_ids()
             rec.allowed_journal_ids = rec.active_allowed_journal_ids - parent_journals
