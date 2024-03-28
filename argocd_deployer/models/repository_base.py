435554b9a4dd69fe6410b7ff0f636dcac1ee19e0
Mark Schuit <mark@gig.solutions>
2024-03-28 13:29:01 +0800

    [FIX] argocd_deployment - Fixed warnings and tests.

--- argocd_deployer/models/repository_base.py
+++ argocd_deployer/models/repository_base.py
@@ -1,7 +1,6 @@
 import logging
 
 from odoo import models
-from odoo.tools.safe_eval import safe_eval
 
 ADD_FILES = "add"
 REMOVE_FILES = "remove"
@@ -11,6 +10,7 @@ _logger = logging.getLogger(__name__)
 
 class RepositoryBase(models.AbstractModel):
     _name = "argocd.repository.base"
+    _description = "Base model for models that interact with repositories"
 
     def _get_repository(self):
         """Get the repository and affected by this model."""
@@ -56,20 +56,22 @@ class RepositoryBase(models.AbstractModel):
         # TODO: Fix concurrency issue
         # TODO: add automatic healing if conflicts appear whatsoever
         self.ensure_one()
-        is_simulation = safe_eval(
+        simulation_mode = (
             self.env["ir.config_parameter"]
             .sudo()
-            .get_param("argocd.simulate_commit", False)
+            .get_param("argocd.git_simulation_mode", "none")
+            .lower()
         )
+
         repository = self._get_repository()
         branch = self._get_branch()
         remote = self._get_remote(repository)
-        if not is_simulation:
+        if "pull" not in simulation_mode:
             _logger.info(f"Pulling from repository {repository.working_dir}...")
             self._pull_from_repository(repository, branch, remote)
         files, message = local_changes_callback()
 
-        if not is_simulation:
+        if "push" not in simulation_mode:
             _logger.info(f"Pushing to repository {repository.working_dir}...")
             self._commit(repository, message, files)
             remote.push()
