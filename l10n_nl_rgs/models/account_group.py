bff10664c1507121a5f9801d0fdeda4440a67f03
ByteMe <157343079+ByteMeAsap@users.noreply.github.com>
2024-04-04 15:12:46 +0530

    Change order for account_group
    Change order for account groups as suggested by Andre Schenkels in onesteinbv/addons-container#89

--- l10n_nl_rgs/models/account_group.py
+++ l10n_nl_rgs/models/account_group.py
@@ -7,7 +7,7 @@ from odoo import fields, models
 
 class AccountGroup(models.Model):
     _inherit = "account.group"
-    _order = "sort_code, code, code_prefix_start"
+    _order = "sort_code, display_name"
 
     # FIXME: This should be English with Dutch translation
     referentiecode = fields.Char()
