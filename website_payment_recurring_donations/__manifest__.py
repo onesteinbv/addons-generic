6f9751576272fa772d163ce74232077ed0e60c0d
Anjeel Haria <bytemeasap@gmail.com>
2024-04-12 20:21:09 +0530

    pre-commit fixes

--- website_payment_recurring_donations/__manifest__.py
+++ website_payment_recurring_donations/__manifest__.py
@@ -1,23 +1,24 @@
 # Copyright 2023 Onestein- Anjeel Haria
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
-    'name': 'Recurring donations',
-    'version': '16.0.1.0.0',
-    'category': 'Website',
-    'license': 'LGPL-3',
-    'summary': 'Recurring donations',
-    'description': """Recurring donations""",
-    'depends': ["website_payment"],
-    'data': [
+    "name": "Recurring donations",
+    "version": "16.0.1.0.0",
+    "category": "Website",
+    "license": "LGPL-3",
+    "summary": "Recurring donations",
+    "description": """Recurring donations""",
+    "website": "https://www.onestein.nl",
+    "depends": ["website_payment"],
+    "data": [
         "data/donation_data.xml",
         "views/snippets/s_donation.xml",
         "views/donation_templates.xml",
         "views/payment_transaction_view.xml",
-        "views/res_partner_view.xml"
+        "views/res_partner_view.xml",
     ],
     "assets": {
-        'web.assets_frontend': [
-            'website_payment_recurring_donations/static/src/js/website_payment_form.js',
+        "web.assets_frontend": [
+            "website_payment_recurring_donations/static/src/js/website_payment_form.js",
         ],
     },
 }
