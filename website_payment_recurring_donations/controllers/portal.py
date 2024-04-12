d79b2375a323fc03a2498000525a1753932cb166
Anjeel Haria <bytemeasap@gmail.com>
2024-04-12 19:09:46 +0530

    [ADD] Added modules website_payment_recurring_donations and website_payment_recurring_donations_mollie

---
+++ website_payment_recurring_donations/controllers/portal.py
@@ -0,0 +1,59 @@
+# Copyright 2023 Onestein - Anjeel Haria
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.addons.payment.controllers import portal as payment_portal
+from odoo import http
+from odoo.http import request
+
+
+class PaymentPortal(payment_portal.PaymentPortal):
+
+    @http.route('/donation/transaction/<minimum_amount>', type='json', auth='public', website=True, sitemap=False)
+    def donation_transaction(self, amount, currency_id, partner_id, access_token, minimum_amount=0, **kwargs):
+        if request.env.user._is_public():
+            kwargs['donation_partner_details'] = kwargs['partner_details']
+        return super().donation_transaction(amount, currency_id, partner_id, access_token, minimum_amount, **kwargs)
+
+    def _get_custom_rendering_context_values(
+            self,
+            donation_options=None,
+            donation_descriptions=None,
+            is_donation=False,
+            **kwargs
+    ):
+        rendering_context = super()._get_custom_rendering_context_values(
+            donation_options=donation_options,
+            donation_descriptions=donation_descriptions,
+            is_donation=is_donation,
+            **kwargs
+        )
+        if is_donation:
+            if kwargs.get('donation_frequency') and kwargs.get('donation_frequency') == 'monthly':
+                is_monthly = True
+            else:
+                is_monthly = False
+            rendering_context.update({
+                'is_onetime': not is_monthly,
+                'is_monthly': is_monthly,
+            })
+        return rendering_context
+
+    def _create_transaction(self, partner_id, *args, custom_create_values=None, **kwargs):
+        if kwargs.get('donation_frequency'):
+            if not custom_create_values:
+                custom_create_values = {}
+            if 'donation_frequency' not in custom_create_values:  # We are in the payment module's flow
+                custom_create_values['donation_frequency'] = kwargs.pop('donation_frequency')
+            if kwargs.get('donation_partner_details'):
+                res_partner_obj = request.env['res.partner'].sudo()
+                details = kwargs.pop('donation_partner_details')
+                country_id = int(details.get('country_id'))
+                email = details.get('email')
+                partner_id = res_partner_obj.search(
+                    [('email', '=ilike', email), ('country_id', '=', country_id)],
+                    limit=1).id
+                if not partner_id:
+                    partner_id = res_partner_obj.create({'name': details.get('name'), 'email': email,
+                                                         'country_id': country_id}).id
+        return super()._create_transaction(partner_id=partner_id, *args, custom_create_values=custom_create_values,
+                                           **kwargs)
---
+++ website_payment_recurring_donations_mollie/models/payment_provider.py
@@ -0,0 +1,29 @@
+# Copyright 2023 Onestein - Anjeel Haria
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from mollie.api.client import Client as MollieClient
+from odoo import models, service
+
+
+class PaymentAcquirerMollie(models.Model):
+    _inherit = 'payment.provider'
+
+    def _api_mollie_create_customer_id(self):
+        if self._context.get('partner', False):
+            partner = self.env['res.partner'].browse(self._context.get('partner'))
+            customer_data = {'name': partner.name, 'metadata': {'odoo_partner_id': partner.id}}
+            if partner.email:
+                customer_data['email'] = partner.email
+            return self._mollie_make_request('/customers', data=customer_data, method="POST")
+        return super(PaymentAcquirerMollie, self)._api_mollie_create_customer_id()
+
+    def _api_mollie_get_client(self):
+        mollie_client = MollieClient(timeout=10)
+        if self.state == 'enabled':
+            mollie_client.set_api_key(self.mollie_api_key)
+        elif self.state == 'test':
+            mollie_client.set_api_key(self.mollie_api_key_test)
+
+        mollie_client.set_user_agent_component('Odoo', service.common.exp_version()['server_version'])
+        mollie_client.set_user_agent_component('MollieOdoo', self.env.ref('base.module_payment_mollie').installed_version)
+        return mollie_client
