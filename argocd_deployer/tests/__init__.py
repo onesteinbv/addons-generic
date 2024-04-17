90868d21ff596c6ac623ba43084bbb43fca5fe6b
Mark Schuit <mark@gig.solutions>
2024-04-17 17:47:58 +0800

    [FIX] Fixed get_domain_yaml_path
    The get_domain_yaml_path did not return a value in all execution paths.

--- argocd_deployer/tests/__init__.py
+++ argocd_deployer/tests/__init__.py
@@ -1,2 +1,3 @@
 from . import test_application
 from . import test_application_set
+from . import test_application_tag
---
+++ argocd_deployer/tests/test_application_tag.py
@@ -0,0 +1,54 @@
+from unittest import skip
+from unittest.mock import MagicMock, patch
+
+from odoo import Command
+from odoo.tests.common import TransactionCase
+
+
+class TestApplicationTag(TransactionCase):
+
+    def test_get_domain_yaml_path(self):
+        """get_domain_yaml_path returns a reasonable value in all circumstances."""
+
+        template = self.env["argocd.application.set.template"].create({
+            "name": "test-set-template",
+            "yaml": """test:
+  me: domain"""
+        })
+        application_set_1 = self.env["argocd.application.set"].create({
+            "name": "test-set",
+            "template_id": template.id,
+            "repository_url": "http://hello.com",
+            "branch": "hello",
+            "repository_directory": "hello",
+            "deployment_directory": "bye",
+        })
+        application_set_2 = self.env["argocd.application.set"].create({
+            "name": "test-set-also",
+            "template_id": template.id,
+            "repository_url": "http://hello.com",
+            "branch": "hello",
+            "repository_directory": "hello",
+            "deployment_directory": "byeagain",
+        })
+        tag = self.env["argocd.application.tag"].create({
+            "name": "test-tag",
+            "domain_yaml_path": False,
+            "key": "thegoldentowerkey"
+        })
+
+        self.assertFalse(tag.get_domain_yaml_path())
+        self.assertFalse(tag.get_domain_yaml_path(application_set_1))
+
+        tag.domain_yaml_path = "test.me"
+        self.assertEqual("test.me", tag.get_domain_yaml_path())
+        self.assertEqual("test.me", tag.get_domain_yaml_path(application_set_1))
+
+        tag.domain_override_ids = [Command.create({
+            "application_set_id": application_set_1.id,
+            "domain_yaml_path": "test.me.now",
+        })]
+
+        self.assertEqual("test.me", tag.get_domain_yaml_path())
+        self.assertEqual("test.me.now", tag.get_domain_yaml_path(application_set_1))
+        self.assertEqual("test.me", tag.get_domain_yaml_path(application_set_2))
