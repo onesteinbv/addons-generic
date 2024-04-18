8486fe873cfc9d1ce7db413fcc52cad57fe433a4
Mark Schuit <mark@gig.solutions>
2024-04-18 20:39:03 +0800

    [ADD] base_list_owned_databases - Allow the list_db selector to see databases with an owner role of which the current user is a member.
    This module needs to be added as a server-wide module.

---
+++ base_list_owned_databases/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2017-2023 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "List Owned Databases",
+    "summary": "When using the database selector, the list of databases "
+               "now consists of any databases the current user has "
+               "ownership of through role membership, rather than only "
+               "those specifically owned by the user. "
+               "Must be declared as a server-wide module.",
+    "author": "Onestein",
+    "website": "https://www.onestein.nl",
+    "category": "Tools",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "installable": True,
+}
