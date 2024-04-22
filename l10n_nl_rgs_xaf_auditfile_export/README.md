e2e38c53eb451dd6fc315a558b673f39ac4887ae
André Schenkels <andre@schenkels.nl>
2024-04-22 12:31:30 +0000

    ADD XAF RGS Module

---
+++ l10n_nl_rgs_xaf_auditfile_export/README.md
@@ -0,0 +1,6 @@
+NL RGS - XAF auditfile export
+=============================
+
+This module integrates the l10n_nl_rgs localization with the l10n_nl_xaf_auditfile_export XAF export module.
+
+It adds the reference code in the leadCode of the export.
---
+++ l10n_nl_rgs_xaf_auditfile_export/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "NL RGS - XAF auditfile export",
+    "version": "16.0.1.0.0",
+    "category": "Accounting/Localizations/Account Charts",
+    "author": "Onestein",
+    "website": "https://www.onestein.nl",
+    "depends": ["l10n_nl_rgs", "l10n_nl_xaf_auditfile_export"],
+    "data": [
+        "views/templates.xml",
+    ],
+    "auto_install": True,
+    "installable": True,
+    "license": "LGPL-3",
+}
