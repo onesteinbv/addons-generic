ed35fca21485014bab0580e56e928bc32c65bf57
Anjeel Haria <bytemeasap@gmail.com>
2024-06-12 19:48:15 +0530

    [ADD] Add crypto_currency module
    pre-commit fixes

---
+++ crypto_currency/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2024 Onestein
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Crypto Currency",
+    "version": "16.0.1.0.0",
+    "author": "Onestein BV",
+    "website": "https://www.onestein.nl",
+    "license": "AGPL-3",
+    "category": "Financial Management/Configuration",
+    "summary": "Allow more characters for currency name and more digits for rounding",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+}
