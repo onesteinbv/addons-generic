599c86d7a7c6a814f55b284a5c51ca20a8809204
tarteo <d.sluijk@onestein.nl>
2024-07-05 14:28:58 +0200

    [ADD][WIP] Improved ordering user interface

--- argocd_website/controllers/main.py
+++ argocd_website/controllers/main.py
@@ -62,8 +62,8 @@ class MainController(Controller):
         return request.render(
             "argocd_website.order",
             {
-                "main_product": product,
-                "main_product_tmpl": product_tmpl,
+                "main_product": product.sudo(),
+                "main_product_tmpl": product_tmpl.sudo(),
                 "current_step": "configure",
             },
         )
