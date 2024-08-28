063ddeb0ee8db086c573fd690851e1a4888a2b49
Dennis Sluijk <10028499+tarteo@users.noreply.github.com>
2024-08-28 09:45:42 +0200

    [ADD] Cleanup old stats cron and create_stats (#64)
    * [ADD] Cleanup old stats cron
    [ADD] create_stats to easily create stats using the external API
    
    * [ADD] Cleanup old stats cron
    [ADD] create_stats to easily create stats using the external API
    
    * [ADD] create_stats to easily create stats using the external API
    
    * [ADD] create_stats to easily create stats using the external API
    
    * [ADD] Security group dedicated for reporting statistics
    
    * [IMP] argocd.application.stat default date value
    
    * [IMP] argocd.application.stat default date value
    
    * [FIX] argocd.application.stat fixup

--- argocd_deployer/models/application_stat.py
+++ argocd_deployer/models/application_stat.py
@@ -1,4 +1,7 @@
-from odoo import fields, models
+from datetime import timedelta
+
+from odoo import api, fields, models
+from odoo.exceptions import MissingError
 
 
 class ApplicationStat(models.Model):
@@ -12,5 +15,70 @@ class ApplicationStat(models.Model):
     type_id = fields.Many2one(
         comodel_name="argocd.application.stat.type", required=True
     )
-    date = fields.Datetime(help="The date when the measurement was done", required=True)
-    value = fields.Float()
+    date = fields.Datetime(
+        default=fields.Datetime.now,
+        help="The date when the measurement was done",
+        required=True,
+    )
+    message = fields.Char()
+    value = fields.Float(digits=(15, 3))
+
+    @api.model
+    def create_stats(self, application_name, stats):
+        """
+        Create argocd.application.stat easier
+
+        @param application_name: name of the application the stats relate to
+        @param stats: list of stats (dicts):
+            - type: Key of argocd.application.stat.type
+            - date: date Odoo formatted (YY-MM-dd HH:mm:ss)
+            - value: float value
+            - message: string value
+        @return: ids of the created records
+        """
+        application = (
+            self.env["argocd.application"]
+            .sudo()
+            .search([("name", "=", application_name)])
+        )  # sudo so we don't have to give access to argocd.application which can contain sensitive information
+        if not application:
+            raise MissingError(
+                "Application with name `%s` doesn't exist" % application_name
+            )
+        type_ids = {}
+        to_create = []
+        for stat in stats:
+            stat_type_key = stat["type"]
+            if stat_type_key not in type_ids:
+                stat_type = self.env["argocd.application.stat.type"].search(
+                    [("key", "=", stat_type_key)]
+                )
+                if not stat_type:
+                    raise MissingError(
+                        "Statistics Type with key `%s` doesn't exist" % stat_type_key
+                    )
+                type_ids[stat_type_key] = stat_type.id
+            type_id = type_ids[stat_type_key]
+            values = {
+                "application_id": application.id,
+                "type_id": type_id,
+                "date": stat["date"],
+                "value": float(stat.get("value", "0")),
+                "message": stat.get("message"),
+            }
+            to_create.append(values)
+
+        return self.create(to_create).ids
+
+    def _cron_cleanup_old_stats(self):
+        retention = int(
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("argocd.application_stat_retention_days", "0")
+        )
+        if not retention:
+            return
+        abandoned_date = fields.Datetime.now() - timedelta(days=retention)
+        self.search(
+            [("date", "<=", fields.Datetime.to_string(abandoned_date))]
+        ).unlink()
