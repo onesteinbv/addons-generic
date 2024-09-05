c01660f3921efaf18593a8cf78874cc82ce17084
tarteo <d.sluijk@onestein.nl>
2024-09-05 13:18:27 +0200

    [ADD] argocd_website: Translations

---
+++ argocd_website/models/ir_http.py
@@ -0,0 +1,10 @@
+from odoo import models
+
+
+class IrHttp(models.AbstractModel):
+    _inherit = "ir.http"
+
+    @classmethod
+    def _get_translation_frontend_modules_name(cls):
+        mods = super(IrHttp, cls)._get_translation_frontend_modules_name()
+        return mods + ["argocd_website"]
