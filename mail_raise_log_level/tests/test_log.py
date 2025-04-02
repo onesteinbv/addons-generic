3c0641c1f63284a719eead69696072b95cbca595
tarteo <d.e.sluijk@gmail.com>
2025-04-02 13:43:26 +0200

    mail_raise_log_level: Fix linting

--- mail_raise_log_level/tests/test_log.py
+++ mail_raise_log_level/tests/test_log.py
@@ -1,24 +1,27 @@
-from odoo.tests.common import TransactionCase
 from unittest.mock import patch
+
+from odoo.tests.common import TransactionCase
 
 
 class TestLog(TransactionCase):
     def test_fetch_mail(self):
         # Create a broken fetchmail server
-        self.env["fetchmail.server"].create({
-            "name": "Broken Server",
-            "server_type": "pop",
-            "server": "test",
-            "port": 110,
-            "user": "test",
-            "password": "test",
-            "state": "done"
-        })
-
+        self.env["fetchmail.server"].create(
+            {
+                "name": "Broken Server",
+                "server_type": "pop",
+                "server": "test",
+                "port": 110,
+                "user": "test",
+                "password": "test",
+                "state": "done",
+            }
+        )
 
         # Check if the log message was captured
-        with self.assertLogs("odoo.addons.mail_raise_log_level.models.fetch_mail_server", level="ERROR") as log:
-
+        with self.assertLogs(
+            "odoo.addons.mail_raise_log_level.models.fetch_mail_server", level="ERROR"
+        ) as log:
             # Simulate cron
             self.env["fetchmail.server"]._fetch_mails()
 
@@ -27,29 +30,34 @@ class TestLog(TransactionCase):
                 log.records[0].message,
             )
 
-    
     def test_failing_outgoing_mail(self):
         # Create a broken outgoing mail server
-        mail_server = self.env["ir.mail_server"].create({
-            "name": "Broken Server",
-            "smtp_host": "test222",
-            "smtp_user": "test",
-            "smtp_pass": "test",
-            "smtp_port": 25
-        })
+        mail_server = self.env["ir.mail_server"].create(
+            {
+                "name": "Broken Server",
+                "smtp_host": "test222",
+                "smtp_user": "test",
+                "smtp_pass": "test",
+                "smtp_port": 25,
+            }
+        )
 
         # Check if the log message was captured
-        with self.assertLogs("odoo.addons.mail_raise_log_level.models.mail_mail", level="ERROR") as log:
-            mail = self.env["mail.mail"].create({
-                "mail_server_id": mail_server.id,
-                "auto_delete": False,
-                "email_to": "test@123.com",
-                "email_from": "admin@123.com",
-                "subject": "Test",
-                "body_html": "<p>Test</p>",
-            })
+        with self.assertLogs(
+            "odoo.addons.mail_raise_log_level.models.mail_mail", level="ERROR"
+        ) as log:
+            mail = self.env["mail.mail"].create(
+                {
+                    "mail_server_id": mail_server.id,
+                    "auto_delete": False,
+                    "email_to": "test@123.com",
+                    "email_from": "admin@123.com",
+                    "subject": "Test",
+                    "body_html": "<p>Test</p>",
+                }
+            )
             IrMailServer = self.env["ir.mail_server"]
-            with patch.object(type(IrMailServer), '_is_test_mode', lambda self: False):
+            with patch.object(type(IrMailServer), "_is_test_mode", lambda self: False):
                 mail.send()
                 self.assertEqual(
                     mail.state,
