fcddf1a12de6663fc53d9dd9e54ee11247554219
tarteo <d.e.sluijk@gmail.com>
2025-05-02 14:06:35 +0200

    argocd_sale: Remove unecessary dependency

--- argocd_sale/__manifest__.py
+++ argocd_sale/__manifest__.py
@@ -10,7 +10,6 @@
         "subscription_oca",
         "argocd_deployer",
         "sale_recurring_payment",
-        "account_invoice_pricelist",
     ],
     "demo": [
         "demo/sale_subscription_template_demo.xml",
