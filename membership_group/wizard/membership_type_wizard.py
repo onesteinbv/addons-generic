59c34bd56778b568cbca35764ca88392eb28190b
Goncalo Brito <gbrito@gdsbb.org>
2025-06-06 12:38:42 +0200

    [REF] remove date_to and check for future

--- membership_group/wizard/membership_type_wizard.py
+++ membership_group/wizard/membership_type_wizard.py
@@ -1,5 +1,4 @@
-from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
+from odoo import api, fields, models
 
 from ..models.membership_group_member import MEMBER_TYPE
 
@@ -29,20 +28,11 @@ class MembershipTypeWizard(models.TransientModel):
         help="Start date of the membership",
         required=True,
     )
-    date_to = fields.Date(
-        string="To",
-        help="Planned end date of the membership",
-    )
 
     @api.depends("date_from")
     def _compute_member_date_to(self):
         for record in self:
             record.member_date_to = fields.Date.subtract(record.date_from, days=1)
-
-    def _check_date_from(self):
-        for record in self:
-            if record.date_to and record.date_to < record.date_from:
-                raise ValidationError(_("Date to needs to be higher than date from"))
 
     def _prepare_new_member_line_values(self):
         self.ensure_one()
@@ -51,18 +41,14 @@ class MembershipTypeWizard(models.TransientModel):
             "group_id": self.member_id.group_id.id,
             "type": self.type,
             "date_from": self.date_from,
-            "date_to": self.date_to,
         }
 
     def action_change_type(self):
         rec_values = []
         for record in self:
-            record._check_date_from()
-            record.member_id.write(
-                {
-                    "date_to": record.member_date_to,
-                }
-            )
+            record.member_id.date_to = record.member_date_to
+            if record.date_from <= fields.Date.context_today(record):
+                record.member_id.date_end = record.member_date_to
             rec_values.append(record._prepare_new_member_line_values())
 
         if rec_values:
