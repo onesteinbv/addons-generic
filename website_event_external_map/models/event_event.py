5cb60b6da2bd2282a26869e64c273b0694ad43a6
Anjeel Haria <bytemeasap@gmail.com>
2025-06-11 20:24:36 +0530

    [MIG] website_event_external_map: Migration to 18.0

---
+++ website_event_external_map/models/event_event.py
@@ -0,0 +1,15 @@
+from odoo import models
+
+
+class EventEvent(models.Model):
+    _inherit = "event.event"
+
+    def map_link(self, zoom=8):
+        return self._map_link(zoom=zoom)
+
+    def _map_link(self, zoom=8):
+        self.ensure_one()
+        res = ""
+        if self.address_id:
+            res = self.address_id.open_map()["url"]
+        return res
