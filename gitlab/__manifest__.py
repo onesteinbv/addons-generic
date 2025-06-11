67fcc93142fe7144bc2eac5b8324109d1f2ae467
Anjeel Haria <bytemeasap@gmail.com>
2025-06-11 20:03:35 +0530

    [MIG] gitlab: Migration to 18.0

---
+++ gitlab/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Gitlab",
+    "category": "Technical",
+    "version": "18.0.1.0.0",
+    "author": "Onestein",
+    "website": "https://www.onestein.nl",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "external_dependencies": {"python": ["gitlab"]},
+    "data": ["security/ir_model_access.xml", "views/gitlab_view.xml", "menuitems.xml"],
+}
---
+++ gitlab/models/gitlab.py
@@ -0,0 +1,37 @@
+from odoo import api, fields, models
+
+import gitlab
+
+
+class Gitlab(models.Model):
+    _name = "gitlab"
+    _description = "Gitlab Connection"
+
+    url = fields.Char(string="URL", default="https://gitlab.com")
+    private_token = fields.Char(string="Private / Personal Token", required=True)
+    debug = fields.Boolean(string="Enable Debug Mode")
+    per_page = fields.Integer(string="Pagination", default=100)
+
+    @api.depends("url")
+    def _compute_display_name(self):
+        for rec in self:
+            rec.display_name = rec.url
+
+    def get_server_connection(self):
+        self.ensure_one()
+        gl = gitlab.Gitlab(url=self.url, private_token=self.private_token)
+        if self.debug:
+            gl.enable_debug()
+        return gl
+
+    def validate(self):
+        gl = self.get_server_connection()
+        gl.auth()
+        return {
+            "type": "ir.actions.client",
+            "tag": "display_notification",
+            "params": {
+                "message": self.env._("Connection Test Successful"),
+                "type": "success",
+            },
+        }
