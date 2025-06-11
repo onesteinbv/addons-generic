a40aeea914ae32d8c6b90d86a95bfa28280bc2da
Anjeel Haria <bytemeasap@gmail.com>
2025-06-11 20:51:01 +0530

    [MIG] website_project_generic_role_members: Migration to 18.0

---
+++ website_project_generic_role_members/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "Website Project Generic Role Members",
+    "summary": "Website Project Generic Role Members",
+    "category": "Website",
+    "version": "18.0.1.0.0",
+    "author": "Onestein",
+    "website": "https://www.onestein.nl",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+        "project_role",
+        "project_role_members",
+        "website",
+        "website_membership",
+        "website_project_generic",
+    ],
+    "data": [
+        "templates/project_project_template.xml",
+        "views/res_partner_view.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "/website_project_generic_role_members/static/src/css/website_project_generic_role_members.css",
+        ],
+    },
+}
