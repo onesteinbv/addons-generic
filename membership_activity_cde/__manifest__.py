558be54507df500f2b9b118c064941c738f6774f
Anjeel Haria <bytemeasap@gmail.com>
2025-06-11 20:08:19 +0530

    [MIG] membership_activity_cde: Migration to 18.0

---
+++ membership_activity_cde/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Membership Activity extension for collaborative development environments",
+    "summary": "Track CDE activity of members e.g. commits, pull requests and reviews",
+    "category": "Membership",
+    "version": "18.0.1.0.0",
+    "author": "Onestein",
+    "website": "https://www.onestein.nl",
+    "license": "AGPL-3",
+    "depends": ["membership_activity"],
+    "data": [
+        "data/membership_activity_type_data.xml",
+    ],
+}
