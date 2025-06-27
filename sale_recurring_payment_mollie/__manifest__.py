ffe577dded85181aeac8358789b3347e5a9bc44c
Anjeel Haria <bytemeasap@gmail.com>
2025-06-27 11:08:18 +0530

    [ADD] Added partner_mollie module and added this modules as dependencies for other modules

--- sale_recurring_payment_mollie/__manifest__.py
+++ sale_recurring_payment_mollie/__manifest__.py
@@ -5,10 +5,9 @@
     "license": "LGPL-3",
     "summary": "Sale - Recurring Payment Mollie",
     "website": "https://www.onestein.nl",
-    "depends": ["payment_mollie_official", "sale_recurring_payment"],
+    "depends": ["payment_mollie_official", "sale_recurring_payment", "partner_mollie"],
     "data": [
         "data/payment_provider.xml",
-        "views/partner_view.xml",
     ],
     "external_dependencies": {"python": ["mollie-api-python"]},
 }
