4d41f5b2cf3a188d80b9bb246b1f6bf94f111fc9
tarteo <d.e.sluijk@gmail.com>
2025-07-04 11:43:02 +0200

    argocd_website: migration issue

--- argocd_website/controllers/payment.py
+++ argocd_website/controllers/payment.py
@@ -4,8 +4,8 @@ from odoo.addons.payment.controllers.portal import PaymentPortal
 
 
 class ArgocdPaymentPortal(PaymentPortal):
-    def _get_custom_rendering_context_values(self, **kwargs):
-        res = super()._get_custom_rendering_context_values(**kwargs)
+    def _get_extra_payment_form_values(self, **kwargs):
+        res = super()._get_extra_payment_form_values(**kwargs)
         is_paying_for_app_subscription = False
         if "invoice_id" in kwargs:
             invoice = request.env["account.move"].sudo().browse(kwargs["invoice_id"])
