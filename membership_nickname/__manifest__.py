2c130092e91c18bf3fe8519e2d637954c1da47af
Anjeel Haria <bytemeasap@gmail.com>
2025-07-10 15:37:30 +0530

    [MIG] membership_nickname: Migration to 18.0

---
+++ membership_nickname/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Members Nickname",
+    "summary": "Adds Nickname for members",
+    "version": "18.0.1.0.0",
+    "author": "Onestein",
+    "website": "https://www.onestein.nl",
+    "license": "AGPL-3",
+    "category": "Extra Tools",
+    "depends": ["partner_nickname", "membership"],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "auto_install": True,
+    "installable": True,
+}
