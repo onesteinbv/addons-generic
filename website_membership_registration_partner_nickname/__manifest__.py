6a156f4f675d0cc27e6bdea09fe7fc20c6846c43
Anjeel Haria <bytemeasap@gmail.com>
2025-07-14 15:48:15 +0530

    [MIG] website_membership_registration_partner_nickname: Migration to 18.0

---
+++ website_membership_registration_partner_nickname/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Website Membership Registration Partner Nickname",
+    "category": "Website",
+    "version": "18.0.1.0.0",
+    "author": "Onestein",
+    "license": "AGPL-3",
+    "website": "https://www.onestein.nl",
+    "depends": ["website_membership_registration", "partner_nickname"],
+    "data": [
+        "templates/website.xml",
+    ],
+    "demo": [
+        "data/res_partner_demo.xml",
+    ],
+    "application": False,
+    "installable": True,
+    "auto_install": True,
+}
