f01f947a3084fa47a33ce9f31429f268658f4586
Anjeel Haria <bytemeasap@gmail.com>
2025-07-14 10:40:36 +0530

    [FIX] website_membership_registration: Changes to handle errors during membership registration
    Updates
    
    Updates

--- website_membership_registration_partner_contact_birthdate/controllers/main.py
+++ website_membership_registration_partner_contact_birthdate/controllers/main.py
@@ -45,6 +45,6 @@ class MembershipRegistrationControllerBirthDate(MembershipRegistrationController
             vals["birthdate_date"] = partner_data["member_birthdate_date"]
         return vals
 
-    def _update_old_data_in_session(self, partner_data, error_message, errors):
+    def _handle_errors(self, partner_data, error_message, errors):
         partner_data.pop("member_birthdate_date", None)
-        return super()._update_old_data_in_session(partner_data, error_message, errors)
+        return super()._handle_errors(partner_data, error_message, errors)
