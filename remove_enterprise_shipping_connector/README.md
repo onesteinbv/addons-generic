520edb8548551553372031b470ea86f2727ad3fa
tarteo <d.sluijk@onestein.nl>
2025-12-05 11:55:10 +0100

    Add remove_enterprise_shipping_connector

---
+++ remove_enterprise_shipping_connector/README.md
@@ -0,0 +1,1 @@
+This module exists to fast track a solution for this: https://github.com/OCA/server-brand/issues/114
---
+++ remove_enterprise_shipping_connector/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Remove Enterprise Shipping Connectors",
+    "version": "18.0.1.0.0",
+    "category": "Maintenance",
+    "author": "Onestein",
+    "license": "AGPL-3",
+    "depends": ["base_setup"],
+}
