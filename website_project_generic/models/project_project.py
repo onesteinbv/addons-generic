1e5bf16098782326f2d818d63f7a4b89fcc57811
Anjeel Haria <bytemeasap@gmail.com>
2026-01-28 11:13:15 +0530

    [UPD] website_project_generic: Added SEO Metadata

--- website_project_generic/models/project_project.py
+++ website_project_generic/models/project_project.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models
 
 class ProjectProject(models.Model):
     _name = "project.project"
-    _inherit = ["project.project", "image.mixin", "website.published.mixin"]
+    _inherit = ["project.project", "image.mixin", "website.published.mixin", "website.seo.metadata"]
 
     short_description = fields.Text()
     description = fields.Html()
