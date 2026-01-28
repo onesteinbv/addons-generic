cb34ae655c0fce3ff2b451a8175e85c7c55ac3f2
Anjeel Haria <bytemeasap@gmail.com>
2026-01-28 14:43:42 +0530

    [UPD] remove_enterprise_shipping_connector: Added author

--- remove_enterprise_shipping_connector/__manifest__.py
+++ remove_enterprise_shipping_connector/__manifest__.py
@@ -3,6 +3,7 @@
     "version": "18.0.1.0.0",
     "category": "Maintenance",
     "author": "Onestein",
+    "website": "https://www.onestein.nl",
     "license": "AGPL-3",
     "depends": ["base_setup"],
 }
