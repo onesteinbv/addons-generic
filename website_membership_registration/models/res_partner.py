f26c366ccad7f6c5d2a0b4ad253ed9d8a8d2f43c
Anjeel Haria <bytemeasap@gmail.com>
2026-01-29 18:43:28 +0530

    [UPD] website_membership_registration: Pass companies email through context to verification email template

--- website_membership_registration/models/res_partner.py
+++ website_membership_registration/models/res_partner.py
@@ -165,7 +165,7 @@ class ResPartner(models.Model):
             "website_membership_registration.verification_email"
         )
 
-        mail_template.with_context(verification_url=verification_url).send_mail(
+        mail_template.with_context(verification_url=verification_url,sender_email=self.env.company.email).send_mail(
             self.id, force_send=True, raise_exception=False
         )
 
