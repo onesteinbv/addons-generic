01a776be09751f370827d1bc4429a186538f6d56
tarteo <d.sluijk@onestein.nl>
2026-02-06 16:23:51 +0100

    [FIX] argocd_deployer: Remove usage of old field namespace_prefix

--- argocd_deployer/models/application.py
+++ argocd_deployer/models/application.py
@@ -193,9 +193,9 @@ class Application(models.Model):
 
     @api.constrains("name")
     def _constrain_name(self):
-        # We actually need to also do this check if `namespace_prefix_id.name` changes, but it never does in practice
+        # We actually need to also do this check if `namespace_prefix` changes, but it never does in practice
         # FIXME: The namespace_prefix is not necessarily part of the app name depends on the application.set.template
-        prefix = self.application_set_id.namespace_prefix_id.name
+        prefix = self.application_set_id.namespace_prefix
         if not re.match(
             "^[a-z0-9-]{1,53}$", prefix + self.name
         ):  # lowercase a to z, 0 to 9 and - (dash) are allowed
