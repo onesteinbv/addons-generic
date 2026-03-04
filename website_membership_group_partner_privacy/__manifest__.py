7cae3880cb49062469ceba997e1c1ba41fb4f61f
Anjeel Haria <bytemeasap@gmail.com>
2026-03-04 15:01:14 +0530

    [UPD] website_membership_group: Changes to show members on website

---
+++ website_membership_group_partner_privacy/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2026 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Website Membership Group Partner Privacy",
+    "category": "Membership",
+    "version": "18.0.1.0.0",
+    "author": "Onestein",
+    "license": "AGPL-3",
+    "website": "https://www.onestein.nl",
+    "depends": [
+        "website_membership_group",
+        "website_partner_privacy",
+    ],
+    "data": [
+        "templates/website.xml",
+    ],
+}
