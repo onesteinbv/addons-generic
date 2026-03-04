bc4febfcc9fb4192844cc5d699d5b5ffd0df57a0
ByteMe <157343079+ByteMeAsap@users.noreply.github.com>
2026-03-04 18:53:17 +0530

    [UPD] website_membership_group: Changes to show members on website (#128)

--- website_membership_group_partner_privacy/__manifest__.py
+++ website_membership_group_partner_privacy/__manifest__.py
@@ -1,19 +1,21 @@
-# Copyright 2020 Onestein (<http://www.onestein.eu>)
+# Copyright 2026 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Website Membership Group",
+    "name": "Website Membership Group Partner Privacy",
     "category": "Membership",
     "version": "18.0.1.0.0",
     "author": "Onestein",
     "license": "AGPL-3",
     "website": "https://www.onestein.nl",
     "depends": [
-        "membership_group",
-        "website_membership",
+        "website_membership_group",
+        "website_partner_privacy",
     ],
     "data": [
-        "views/membership_group_view.xml",
         "templates/website.xml",
     ],
+    "auto_install": True,
+    "installable": True,
+    "application": False,
 }
