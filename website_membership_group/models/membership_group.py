59cc72df592426b29551929abefafbdc684e60f0
Anjeel Haria <bytemeasap@gmail.com>
2026-03-27 14:27:45 +0530

    [UPD] website_membership_group: Added snippet options to customize the membership group page
    Updates

--- website_membership_group/models/membership_group.py
+++ website_membership_group/models/membership_group.py
@@ -7,7 +7,7 @@ from odoo.tools.translate import html_translate
 
 class MembershipGroup(models.Model):
     _name = "membership.group"
-    _inherit = ["membership.group", "website.published.mixin"]
+    _inherit = ["membership.group", "website.published.mixin", "website.seo.metadata"]
 
     website_top = fields.Html(strip_style=True, translate=html_translate)
     website_bottom = fields.Html(strip_style=True, translate=html_translate)
@@ -15,6 +15,22 @@ class MembershipGroup(models.Model):
     icon = fields.Image()
     page_id = fields.Many2one(comodel_name="website.page")
     website_url = fields.Char(compute="_compute_website_url", store=True)
+    website_snippet_wrap_classes = fields.Char(
+        string="Wrap Classes",
+        default="mg_wrap mg_show_image mg_show_top mg_show_bottom mg_show_committee mg_show_team",
+        help="Classes for the #wrap element (visibility options)",
+    )
+    website_snippet_members_classes = fields.Char(
+        string="Members Row Classes",
+        default="mg_layout_grid mg_show_committee_desc mg_show_team_desc",
+        help="Classes for the #mg_members_row element (layout and description options)",
+    )
+    website_committee_col_classes = fields.Char(
+        string="Committee Column Classes", default="col-lg-4"
+    )
+    website_team_col_classes = fields.Char(
+        string="Team Column Classes", default="col-lg-8"
+    )
 
     @api.depends("page_id", "page_id.is_published")
     def _compute_website_url(self):
