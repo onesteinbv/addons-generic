04ef64734262c6d7e72eddd46ebb301b46cd143d
ByteMe <157343079+ByteMeAsap@users.noreply.github.com>
2026-05-06 15:07:27 +0530

    [IMP] website_membership_group: Added options to customize the membership group page (#131)

--- website_membership_group/__manifest__.py
+++ website_membership_group/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Website Membership Group",
     "category": "Membership",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.3",
     "author": "Onestein",
     "license": "AGPL-3",
     "website": "https://www.onestein.nl",
@@ -15,5 +15,15 @@
     "data": [
         "views/membership_group_view.xml",
         "templates/website.xml",
+        "views/snippets/snippet_options.xml",
     ],
+    "assets": {
+        "web.assets_frontend": [
+            "website_membership_group/static/src/scss/membership_group.scss",
+            "website_membership_group/static/src/js/membership_group_frontend.esm.js",
+        ],
+        "website.assets_wysiwyg": [
+            "website_membership_group/static/src/js/membership_group_options.esm.js",
+        ],
+    },
 }
