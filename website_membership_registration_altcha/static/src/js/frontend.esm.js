c74b84792a685f851725e616fb42901f25b3d5fa
tarteo <d.sluijk@onestein.nl>
2026-06-10 11:16:08 +0200

    [ADD] website_membership_registration_altcha: Glue module for `website_membership_registration` and `website_altcha`.

---
+++ website_membership_registration_altcha/static/src/js/frontend.esm.js
@@ -0,0 +1,26 @@
+import publicWidget from "@web/legacy/js/public/public_widget";
+import {Altcha} from "@website_altcha/altcha/altcha.esm";
+import {renderToString} from "@web/core/utils/render";
+
+
+publicWidget.registry.WebsiteMembershipRegistration.include({
+    init: function() {
+        this._super(...arguments);
+        this._altcha = new Altcha();
+    },
+
+    willStart: async function() {
+        this._altcha.loadLibs();
+        return this._super(...arguments);
+    },
+
+    start: function() {
+        const $form = this.$el.find("form");
+        if (this._altcha._publicKey && !$form.find(".o_altcha_widget").length) {
+            $form
+                .find("button").last()  // For a lack of a better selector
+                .before(renderToString("website_altcha.AltchaWidget", {}));
+        }
+        return this._super(...arguments);
+    },
+});
