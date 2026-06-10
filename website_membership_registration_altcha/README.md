727c6ca4f04fb363fd2ed2cb0225c93a88a219a1
tarteo <d.sluijk@onestein.nl>
2026-06-10 11:16:08 +0200

    [ADD] website_membership_registration_altcha: Glue module for `website_membership_registration` and `website_altcha`.

---
+++ website_membership_registration_altcha/README.md
@@ -0,0 +1,1 @@
+Glue module for `website_membership_registration` and `website_altcha`.
---
+++ website_membership_registration_altcha/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Website Membership Registration Altcha",
+    "category": "Website",
+    "version": "18.0.1.0.0",
+    "author": "Onestein",
+    "license": "AGPL-3",
+    "website": "https://www.onestein.nl",
+    "depends": ["website_membership_registration", "website_altcha"],
+    "assets": {
+        "web.assets_frontend": [
+            "website_membership_registration_altcha/static/src/js/frontend.esm.js",
+        ],
+    },
+    "auto_install": True,
+}
