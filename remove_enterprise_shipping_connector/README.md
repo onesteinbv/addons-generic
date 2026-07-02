70fa1d6e1bee2ba2e2becc69970aa14a157bb860
tarteo <d.sluijk@onestein.nl>
2026-07-02 11:06:54 +0200

    [FIX] remove_enterprise_shipping_connector: pre-commit issues

--- remove_enterprise_shipping_connector/README.md
+++ remove_enterprise_shipping_connector/README.md
@@ -1,1 +1,2 @@
-This module exists to fast track a solution for this: https://github.com/OCA/server-brand/issues/114
+This module exists to fast track a solution for this:
+https://github.com/OCA/server-brand/issues/114
--- remove_enterprise_shipping_connector/__manifest__.py
+++ remove_enterprise_shipping_connector/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "18.0.1.0.0",
     "category": "Maintenance",
     "author": "Onestein",
-    "website": "https://www.onestein.nl",
+    "website": "https://onestein.nl",
     "license": "AGPL-3",
     "depends": ["base_setup"],
 }
