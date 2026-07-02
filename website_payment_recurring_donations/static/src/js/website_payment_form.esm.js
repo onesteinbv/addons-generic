cd823c825569e1088bd401e43110a6314080eadc
tarteo <d.sluijk@onestein.nl>
2026-07-02 12:45:06 +0200

    [FIX] website_payment_recurring_donations: pre-commit issues

--- website_payment_recurring_donations/static/src/js/website_payment_form.esm.js
+++ website_payment_recurring_donations/static/src/js/website_payment_form.esm.js
@@ -1,15 +1,19 @@
 /* Copyright 2023 Onestein - Anjeel Haria
  * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl). */
 
-import PaymentForm from '@payment/js/payment_form';
+import PaymentForm from "@payment/js/payment_form";
 
 PaymentForm.include({
     _prepareTransactionRouteParams() {
         const transactionRouteParams = this._super(...arguments);
-        // eslint-disable-next-line no-undef
-        return document.querySelector('.o_donation_payment_form') ? {
-            ...transactionRouteParams,
-            'donation_frequency':this.$('input[name="donation_frequency"]:checked').val(),
-        } : transactionRouteParams;
+
+        return document.querySelector(".o_donation_payment_form")
+            ? {
+                  ...transactionRouteParams,
+                  donation_frequency: this.$(
+                      'input[name="donation_frequency"]:checked'
+                  ).val(),
+              }
+            : transactionRouteParams;
     },
 });
