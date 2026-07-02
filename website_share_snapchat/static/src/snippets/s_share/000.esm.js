3f12c9bbcb36650203e428c4697b0c96ac0c31c0
tarteo <d.sluijk@onestein.nl>
2026-07-02 12:45:07 +0200

    [FIX] website_share_snapchat: pre-commit issues

--- website_share_snapchat/static/src/snippets/s_share/000.esm.js
+++ website_share_snapchat/static/src/snippets/s_share/000.esm.js
@@ -1,25 +1,24 @@
-import publicWidget from '@web/legacy/js/public/public_widget';
+import publicWidget from "@web/legacy/js/public/public_widget";
 publicWidget.registry.share.include({
+    /**
+     * @override
+     */
+    start() {
+        this._super(...arguments);
 
-        /**
-         * @override
-         */
-        start() {
-            this._super(...arguments);
+        const urlRegex = /(\?(?:|.*&)(?:attachmentUrl)=)(.*?)(&|#|$)/;
 
-            const urlRegex = /(\?(?:|.*&)(?:attachmentUrl)=)(.*?)(&|#|$)/;
-            // eslint-disable-next-line no-undef
-            const url = encodeURIComponent(window.location.href);
+        const url = encodeURIComponent(window.location.href);
 
-            this.$('a').each((index, element) => {
-                const $a = $(element);
-                if ($a.hasClass('s_share_snapchat')) {
-                    $a.attr('href', (i, href) => {
-                        return href.replace(urlRegex, (match, a, b, c) => {
-                            return a + url + c;
-                        });
+        this.$("a").each((index, element) => {
+            const $a = $(element);
+            if ($a.hasClass("s_share_snapchat")) {
+                $a.attr("href", (i, href) => {
+                    return href.replace(urlRegex, (match, a, b, c) => {
+                        return a + url + c;
                     });
-                }
-            });
-        },
-    });
+                });
+            }
+        });
+    },
+});
