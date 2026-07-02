3238a105047949d404320f563373c41a2b50926f
tarteo <d.sluijk@onestein.nl>
2026-07-02 11:06:54 +0200

    [FIX] membership_activity_github: pre-commit issues

--- membership_activity_github/__manifest__.py
+++ membership_activity_github/__manifest__.py
@@ -3,7 +3,7 @@
     "category": "Membership",
     "version": "18.0.1.0.0",
     "author": "Onestein",
-    "website": "https://www.onestein.nl",
+    "website": "https://onestein.nl",
     "license": "AGPL-3",
     "depends": ["membership_activity", "membership_activity_cde", "queue_job"],
     "external_dependencies": {"python": ["PyGithub==1.59.0"]},
