48bbf41ac2fd90a2528ea13f0c78ed5de8f7381f
tarteo <d.sluijk@onestein.nl>
2026-07-02 12:45:06 +0200

    [FIX] website_partner_privacy_nickname: pre-commit issues

--- website_partner_privacy_nickname/static/src/js/portal.esm.js
+++ website_partner_privacy_nickname/static/src/js/portal.esm.js
@@ -7,12 +7,11 @@ portalDetails.include({
     _onWebsitePrivacyChange: function () {
         this._super();
         var $website_privacy = this.$('select[name="website_privacy"]');
-        var website_privacy = ($website_privacy.val() || "");
-        if(website_privacy === "nickname"){
-            this.$('input[name="nickname"]').prop('required', true);
-        }
-        else{
-            this.$('input[name="nickname"]').prop('required', false);
+        var website_privacy = $website_privacy.val() || "";
+        if (website_privacy === "nickname") {
+            this.$('input[name="nickname"]').prop("required", true);
+        } else {
+            this.$('input[name="nickname"]').prop("required", false);
         }
     },
 });
