b5882c677a55df0ebfe15753af1fd1290a08fe99
tarteo <d.sluijk@onestein.nl>
2026-07-02 12:45:06 +0200

    [FIX] website_partner_privacy: pre-commit issues

--- website_partner_privacy/__manifest__.py
+++ website_partner_privacy/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": "Adds an option to define website privacy options for partners",
     "version": "18.0.1.0.0",
     "author": "Onestein",
-    "website": "https://www.onestein.nl",
+    "website": "https://onestein.nl",
     "license": "AGPL-3",
     "category": "Website",
     "depends": ["website_partner"],
