42b044180147de5eb42be83b8b2be791944cc0f3
tarteo <d.sluijk@onestein.nl>
2026-07-10 13:54:48 +0200

    [FIX] remove_enterprise_shipping_connector: Use view inheritance instead of overriding `get_views`

--- remove_enterprise_shipping_connector/__manifest__.py
+++ remove_enterprise_shipping_connector/__manifest__.py
@@ -1,9 +1,11 @@
 {
     "name": "Remove Enterprise Shipping Connectors",
-    "version": "18.0.1.0.0",
-    "category": "Maintenance",
+    "version": "18.0.1.0.1",
+    "category": "Hidden",
     "author": "Onestein",
     "website": "https://onestein.nl",
     "license": "AGPL-3",
-    "depends": ["base_setup"],
+    "depends": ["stock"],
+    "data": ["views/res_config_settings_view.xml"],
+    "auto_install": True,
 }
